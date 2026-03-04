fcff2d520745b393416ac7ae64226b2e93d1203b
Bertin <7010972@spsd.sk.ca>
2026-03-04 15:15:26 -0600

    ball array update

--- ball-array/sketch.js
+++ ball-array/sketch.js
@@ -2,6 +2,8 @@
 
 let balls = [];
 
+let moveSpeed = 5;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -12,21 +14,35 @@ function draw() {
     //move
     ball.x += ball.dx;
     ball.y += ball.dy;
+    fill(ball.col)
     circle(ball.x, ball.y, ball.radius);
+    if (ball.x > width  + ball.radius) {
+      ball.x = 0 - ball.radius;
+    }
+    else if (ball.x < 0 - ball.radius) {
+      ball.x = width  + ball.radius;
+    }
+    else if (ball.y > height + ball.radius) {
+      ball.y = 0 - ball.radius;
+    }
+    else if (ball.y < 0 - ball.radius) {
+      ball.y = height + ball.radius;
+    }
   }
 }
 
-function spawnball() {
+function spawnball(_x, _y) {
   let theBall = {
-    x: random(width),
-    y: random(height),
-    dx: random(-5, 5),
-    dy: random(-5, 5),
-    radius: random(10, 40)
+    x: _x,
+    y: _y,
+    dx: random(-moveSpeed, moveSpeed),
+    dy: random(-moveSpeed, moveSpeed),
+    radius: random(10, 40),
+    col: color(random(255), random(255), random(255))
   };
   balls.push(theBall);
 }
 
 function mousePressed() {
-  spawnball();
+  spawnball(mouseX, mouseY);
 }
